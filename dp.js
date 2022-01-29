a841d159bd466ebf0a76cfc30faa395036529493
jaylino-1977 <54541679+jaylino-1977@users.noreply.github.com>
2022-01-28 19:13:03 -0600

    Variables assignment

--- dp.js
+++ dp.js
@@ -1,1 +1,23 @@
-console.log("Do you enjoy teaching?")
+var graphics = 1000;
+graphics = graphics + 1000
+
+
+var calligraphy = "font design"; //This is a global variable
+var photography = "head shot"; //This is a global variable
+var videography = "commercial production" //This is a global variable
+
+{ 
+   var graphics = 1000; //This is a block variable
+}
+
+let c = 1000 + 2000;
+let d = 3000 * 2;
+let e = 4000 - 500;
+let f = 6000 / 3000;
+
+let g = "juan" + " " + "Perez";
+
+const camera = {type:"Mirrorless", model:"CanonR6", color:"black"};
+
+
+console.log(graphics); 
